69e9efab18397ee9c3811b63d5446f3ed0213275
RodrigoVanzelotti <rodrigovanzelotti@gmail.com>
2023-04-11 01:52:11 -0300

    basic logic done

--- caneca_spam.py
+++ caneca_spam.py
@@ -16,4 +16,22 @@ chrome_options = Options()
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 driver.get("https://10minutemail.com/")
 
+time.sleep(5)
+email = driver.find_element(By.ID, 'mail_address').get_attribute('value')
+
+driver.execute_script('''window.open("https://thenewscc.com.br/indicacao/?grsf=c0r3yl","_blank");''')
+time.sleep(5)
+driver.find_element(By.ID, 'form-field-email').send_keys(email)
+driver.find_element(By.XPATH, '//*[contains(text(), "confirmar")]').click()
+
+driver.switch_to.window(driver.window_handles[0])
+
+
+driver.close()
+driver.quit()
+
+# driver.get('https://thenewscc.com.br/indicacao/?grsf=c0r3yl')
+
+
+
 time.sleep(15)
