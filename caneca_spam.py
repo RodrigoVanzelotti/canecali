21eef8905ebb96ac3808995bb07fdc381904859f
RodrigoVanzelotti <rodrigovanzelotti@gmail.com>
2023-04-11 14:26:16 -0300

    10minutemail não recebe os emails por ser um software automatizando, procurar outra solução

--- caneca_spam.py
+++ caneca_spam.py
@@ -12,23 +12,46 @@ import json
 import os
 
 chrome_options = Options()
-
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 driver.get("https://10minutemail.com/")
 
-time.sleep(5)
-email = driver.find_element(By.ID, 'mail_address').get_attribute('value')
+time.sleep(10)
+try:
+    email = driver.find_element(By.ID, 'mail_address').get_attribute('value')
+except:    
+    time.sleep(10)
+    email = driver.find_element(By.ID, 'mail_address').get_attribute('value')
 
 driver.execute_script('''window.open("https://thenewscc.com.br/indicacao/?grsf=c0r3yl","_blank");''')
-time.sleep(5)
-driver.find_element(By.ID, 'form-field-email').send_keys(email)
-driver.find_element(By.XPATH, '//*[contains(text(), "confirmar")]').click()
+driver.switch_to.window(driver.window_handles[1])
+continuar = False
+while not continuar:
+    try:
+        driver.find_element(By.NAME, 'form_fields[email]').send_keys(email)
+        driver.find_element(By.XPATH, '//*[contains(text(), "confirmar")]').click()
+        continuar = True
+    except:
+        time.sleep(5)
+
+time.sleep(20)
+continuar = False
+while not continuar:
+    try:
+        driver.find_element(By.CLASS_NAME, 'small_sender').click()
+        driver.find_element(By.XPATH, '//*[contains(text(), "confirmar")]').click()
+        continuar = True
+    except:
+        time.sleep(10)
+
 
 driver.switch_to.window(driver.window_handles[0])
 
 
 driver.close()
 driver.quit()
+
+
+
 
 # driver.get('https://thenewscc.com.br/indicacao/?grsf=c0r3yl')
 
